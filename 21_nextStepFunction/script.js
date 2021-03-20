2947c017106234cc34f4f364c1d30c74d9f1f9ff
Shizuka Oiwa <shizukaoiw@gmail.com>
2021-03-19 23:12:01 -0400

    Add files about JS function advanced

---
+++ 21_nextStepFunction/script.js
@@ -0,0 +1,29 @@
+// exercise 1
+
+// let square = function(num) {
+//     return num * num;
+// }
+
+
+//  exercise 2
+
+// const square = {
+//     area(num) {
+//         return num * num;
+//     },
+//     perimeter(num) {
+//         return num * 4;
+//     }
+// }
+
+
+// exercise 3
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount = this.eggCount + 1;
+        return "EGG";
+    }
+}
