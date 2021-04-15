f0f54c10915572ce2fdcb8d231f26dd55aa14bfd
Shizuka0iwa <shizukaoiw@gmail.com>
2021-04-14 22:48:07 -0400

    Add files about Introduction of Creating Server with Express

---
+++ 33_creatingServersWithExpress/FirstApp/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express()
+
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!!")
+//     res.send("HELLO, WE GOT YOUR REQEST! THIS IS A RESPONSE!")
+//     res.send('<h1>This is my webpage!</h1>')
+// })
+app.get('/', (req, res) => {
+    res.send('This is the home page!!!')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = (req.params);
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = (req.params);
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQEST TO /cats!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!!')
+})
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send('NOTHING FOUND!')
+    }
+    res.send(`<h1>Search results for: ${q}</h1>`)
+})
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path!`)
+})
+
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000!")
+})
